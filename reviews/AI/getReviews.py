948bc2849217bc2199418f8f95aa7722413be153
Abi <abicharan@DESKTOP-TL3LBAQ.localdomain>
2019-10-31 10:40:32 +0800

    Fixed get_reviews func

--- reviews/AI/getReviews.py
+++ reviews/AI/getReviews.py
@@ -2,12 +2,17 @@ import json
 import requests 
 
 
-def get_reviews(str(appid), num_iterations = 1):
-    response = requests.get("https://store.steampowered.com/appreviews/"+appid+"?json=1&num_per_page=100")
-    response = response.json()
-    print(response["reviews"])
+def get_reviews(appid, num_iterations = 0):
+
+    cursor = ""
+    i = 0
+    while i < num_iterations:
+        response = requests.get("https://store.steampowered.com/appreviews/"+str(appid)+"?json=1&num_per_page=100&cursor="+str(cursor))
+        response = response.json()
+        cursor = response["cursor"]
+        print(cursor)
+        i+=1
 
 
 
-
-get_reviews(779340)
+get_reviews(779340, 400)
