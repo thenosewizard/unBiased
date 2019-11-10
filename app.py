9f052c5143c864e7dd645bda9235111316064a9c
Jun Ting Jeffrey Peh <s10186644@connect.np.edu.sg>
2019-11-10 16:53:37 +0800

    Completed login & register without proper UI & db

--- app.py
+++ app.py
@@ -2,4 +2,4 @@ from flask import Flask, render_template
 from reviews import app 
 
 if __name__ == "__main__":
-   app.run()
+   app.run(debug=True)
--- reviews/main/controllers.py
+++ reviews/main/controllers.py
@@ -1,7 +1,28 @@
-from flask import Blueprint, render_template
+from flask import Blueprint, render_template, flash, redirect, url_for
+from reviews.main.forms import RegistrationForm, LoginForm
 main = Blueprint('main', __name__, template_folder= "templates")
 
 
 @main.route('/')
 def index():
     return render_template('index.html')
+
+
+@main.route("/register", methods = ['GET', 'POST'])
+def register():
+    form = RegistrationForm()
+    if form.validate_on_submit():
+        flash(f"Account created for {form.email.data}!","success")
+        return redirect(url_for('main.login'))   
+    return render_template("register.html",title="register",form=form)
+
+@main.route("/login", methods = ['GET','POST'])
+def login():
+    form = LoginForm()
+    if form.validate_on_submit():
+        if form.email.data == "Email@email.com" and form.password.data == "12345678":
+            flash(f"Welcome, {form.email.data}!", "success")
+            return redirect(url_for("main.index"))
+        else:
+            flash("Incorrect email or password!","danger")
+    return render_template("login.html",title="Login", form=form)
