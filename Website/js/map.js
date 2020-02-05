217a508b79bd471425e3557924e1cc6c25ff1322
J370 <jetong2001@gmail.com>
2020-02-05 12:38:10 +0800

    Updates to requirements.txt

--- Website/js/map.js
+++ Website/js/map.js
@@ -1,39 +1,17 @@
-var map;
-function init() {
-    
-    // The overlay layer for our marker
-    var overlay = new OpenLayers.Layer.Vector('Overlay', {
-        styleMap: new OpenLayers.StyleMap({
-            externalGraphic: '../media/maps-and-flags.png',
-            graphicWidth: 20, graphicHeight: 24, graphicYOffset: -24,
-            title: '${tooltip}'
-        })
-    });
+import 'ol/ol.css';
+import {Map, View} from 'ol';
+import TileLayer from 'ol/layer/Tile';
+import OSM from 'ol/source/OSM';
 
-    // The location of our marker and popup. We usually think in geographic
-    // coordinates ('EPSG:4326'), but the map is projected ('EPSG:3857').
-    var myLocation = new OpenLayers.Geometry.Point(10.2, 48.9)
-        .transform('EPSG:4326', 'EPSG:3857');
-
-    // We add the marker with a tooltip text to the overlay
-    overlay.addFeatures([
-        new OpenLayers.Feature.Vector(myLocation, {tooltip: 'OpenLayers'})
-    ]);
-
-    // A popup with some information about our location
-    var popup = new OpenLayers.Popup.FramedCloud("Popup", 
-        myLocation.getBounds().getCenterLonLat(), null,
-        '<a target="_blank" href="http://openlayers.org/">We</a> ' +
-        'could be here.<br>Or elsewhere.', null,
-        true // <-- true if we want a close (X) button, false otherwise
-    );
-
-    // Finally we create the map
-    map = new OpenLayers.Map({
-        div: "map", projection: "EPSG:3857",
-        layers: [new OpenLayers.Layer.OSM(), overlay],
-        center: myLocation.getBounds().getCenterLonLat(), zoom: 15
-    });
-    // and add the popup to it.
-    map.addPopup(popup);
-}
+const map = new Map({
+  target: 'map',
+  layers: [
+    new TileLayer({
+      source: new OSM()
+    })
+  ],
+  view: new View({
+    center: [0, 0],
+    zoom: 0
+  })
+});
