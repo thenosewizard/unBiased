e146256366a629cdfccd314221fd56ae0eec0fce
J370 <jetong2001@gmail.com>
2020-02-06 10:34:26 +0800

    Shifting map

--- reviews/main/controllers.py
+++ reviews/main/controllers.py
@@ -123,5 +123,6 @@ def removeExtra(i):
 
 @main.route('/food')
 def foodIndex():
-    food = Item.query.filter(Item.itemType=="Food").limit(3).all()
-    return render_template("foodIndex.html", title="Index", food=food)
+    carousell = Item.query.filter(Item.itemType=="Food").limit(3).all()
+    food = Item.query.filter(Item.itemType=="Food").all()
+    return render_template("foodIndex.html", title="Index", carousell=carousell, food=food)
